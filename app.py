dc50595ab83f12cc039f11f5032d1315bd1dbdb0
MyNameIsPedro <163777405+RipPedro20@users.noreply.github.com>
2026-03-19 13:08:36 +0000

    Adicionando html

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo Desgramento'
+    return render_template('index.html')
+
+@app.route('/faleconosco')
+@app.route('/email')
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('curriculo.html')
+
+@app.route('/disciplinas')
+def disciplinas():
+    return render_template('disciplinas.html')
+
+@app.route('/acessol')
+def acessol():
+    return render_template('acesSol.html')
+
+if __name__ == '__main__':
+    app.run()

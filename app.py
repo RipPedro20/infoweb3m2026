7325a02ea7bfd5614ff47c6953d5dd17183866db
MyNameIsPedro <163777405+RipPedro20@users.noreply.github.com>
2026-04-16 12:49:51 +0000

    Aula 4 - PAW

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime 
 
 app = Flask(__name__)
 
@@ -35,9 +36,26 @@ def usuario(nome, sobrenome):
 def dados():
     return render_template('dados.html')
 
-@app.route('/recebedados')
+@app.route('/recebedados', methods=['POST'])
 def recebedados():
-    return 'ola'
+    nome = request.form.get('nome')
+    sobrenome = request.form['sobrenome']
+    email = request.form['email']
+    datanasc = request.form['datanasc']
+    estado = request.form.get('estado')
+    escola = request.form.getlist('escola')
+    # data_objeto = datetime.strptime(datanasc, "%Y-%m-%d")
+    # data_formatada = data_objeto.strptime("%d/%m/%Y")
+    return render_template('recebedados.html', nome=nome, sobrenome=sobrenome, email=email, datanasc=datanasc, estado=estado, escola=escola)
+
+@app.route('/compras')
+def compras():
+    return render_template('compras.html')
+
+@app.route('/recebecompras', methods=['POST'])
+def recebecompras():
+    itens = request.form.getlist('item')
+    return render_template('lista.html', itens=itens)
 
 if __name__ == '__main__':
     app.run()
